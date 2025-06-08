e09410274dc12badc5dbc33a2ef9f75ffed3f4b5
Vasili2797 <nashvilliv1@gmail.com>
2025-06-08 04:37:08 -0400

    Added backend for finished Step 5.1

---
+++ Part5/Blog-list-Step4.19/controllers/users.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("blogs", { title: 1, author: 1 });
+  response.json(users);
+});
+
+// Implement a way to create new users by doing an HTTP POST request to address api/users. Users have a username, password and name.
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
