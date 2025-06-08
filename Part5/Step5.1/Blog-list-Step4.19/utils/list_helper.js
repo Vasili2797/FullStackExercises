9a2b95af5e7c2d4bd0b49559ba324d6833a71c8d
Vasili2797 <nashvilliv1@gmail.com>
2025-06-08 13:09:58 -0400

    Step 5.1 solution

---
+++ Part5/Step5.1/Blog-list-Step4.19/utils/list_helper.js
@@ -0,0 +1,49 @@
+const _ = require("lodash");
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  let totalLikes = blogs.reduce(function (sum, order) {
+    return sum + order.likes;
+  }, 0);
+
+  return totalLikes;
+};
+
+const favoriteBlog = (blogs) => {
+  let total = 0;
+  let blog = {};
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes > total) {
+      total = blogs[i].likes;
+      blog = blogs[i];
+    }
+  }
+  console.log(blog);
+  return total;
+};
+
+const mostBlogs = (blogs) => {
+  let mostBlogsWrittenUser = _.countBy(blogs, (blog) => blog.author);
+  const keyValue = Object.keys(mostBlogsWrittenUser).map((key) => [
+    key,
+    mostBlogsWrittenUser[key],
+  ]);
+  keyValue.sort((a, b) => {
+    return b[1] - a[1];
+  });
+  const result = {
+    author: keyValue[0][0],
+    blogs: keyValue[0][1],
+  };
+
+  return result;
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+};
---
+++ Part5/Step5.1/bloglist-frontend/src/App.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [newBlog, setNewBlog] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+    console.log("logged in with", username, password);
+  };
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <div>
+          password
+          <input
+            type="text"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    );
+  };
+
+  const addBlog = (e) => {
+    e.preventDefault();
+
+    const blogObject = {};
+
+    blogService.create(blogObject).then((returnedObject) => {
+      setBlogs(blogs.concat(returnedObject));
+      setNewBlog("");
+    });
+  };
+
+  const handleBlogChange = (e) => {
+    setNewBlog(e.target.value);
+  };
+
+  const blogForm = () => {
+    return (
+      <form onSubmit={addBlog}>
+        <input type="text" value={newBlog} onChange={handleBlogChange} />
+        <button type="submit">save</button>
+      </form>
+    );
+  };
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {loginForm()}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div>
+        <p>{user.name} logged-in</p>
+        {blogForm()}
+      </div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
