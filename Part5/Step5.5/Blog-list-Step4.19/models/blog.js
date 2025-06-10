cedd742b4e40e92237f8aa791d8b0e5d9f98d915
Vasili2797 <nashvilliv1@gmail.com>
2025-06-10 19:53:51 -0400

    Added url property in the blogSchema

--- Part5/Step5.5/Blog-list-Step4.19/models/blog.js
+++ Part5/Step5.5/Blog-list-Step4.19/models/blog.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
+  url: String,
   likes: Number,
   user: {
     type: mongoose.Schema.Types.ObjectId,
