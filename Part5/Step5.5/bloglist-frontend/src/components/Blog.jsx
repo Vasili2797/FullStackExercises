2dfe61c52c1ae9d0477ee874ef104b6578dfe9c2
Vasili2797 <nashvilliv1@gmail.com>
2025-06-16 13:37:51 -0400

    Added blog component

--- Part5/Step5.5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/Step5.5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,64 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+import "../index.css";
+import blogService from "../services/blogs";
 
-export default Blog
+const Blog = ({ blog, updateBlog }) => {
+  const [view, setView] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  if (view) {
+    return (
+      <div style={blogStyle}>
+        <div>Title: {blog.title}</div>
+        <div>Author: {blog.author}</div>
+        <div>
+          Likes: {blog.likes}
+          <button
+            style={{ marginLeft: 10 }}
+            onClick={async () => {
+              const updated = { ...blog, likes: blog.likes + 1 };
+              const updatedBlog = await blogService.update(blog.id, updated);
+              updateBlog(updatedBlog);
+            }}
+          >
+            like
+          </button>
+        </div>
+        {blog.url && <div>URL: {blog.url}</div>}
+        <button
+          className="btn"
+          style={{ marginTop: 5 }}
+          onClick={() => {
+            setView(!view);
+          }}
+        >
+          hide
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button
+          className="btn"
+          onClick={() => {
+            setView(!view);
+          }}
+        >
+          view
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Blog;
