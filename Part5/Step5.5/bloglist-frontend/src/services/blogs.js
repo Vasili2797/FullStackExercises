ca874fe540e0a3366465a4375c8b5705d2664e89
Vasili2797 <nashvilliv1@gmail.com>
2025-12-24 15:03:21 -0500

    Fixed persistent backend issues for step 5.6 and added authconfig in each API call. Ready to go onto Step 5.6

--- Part5/Step5.5/bloglist-frontend/src/services/blogs.js
+++ Part5/Step5.5/bloglist-frontend/src/services/blogs.js
@@ -7,35 +7,29 @@ const setToken = (newToken) => {
   token = `Bearer ${newToken}`;
 };
 
-const getAll = () => {
-  const config = {
-    headers: {
-      Authorization: token,
-    },
-  };
+const authConfig =()=> ({
+  headers: {
+    Authorization: token,
+  },
+});
 
-  const response = axios.get(baseUrl);
-  return response.then((response) => response.data);
-};
-
-const create = async (newObject) => {
-  const config = {
-    headers: {
-      Authorization: token,
-    },
-  };
-
-  const response = await axios.post(baseUrl, newObject, config);
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
   return response.data;
 };
 
+const create = async (newObject) => {
+ const response =await axios.post(baseUrl, newObject, authConfig());
+ return response.data;
+};
+
 const update = async (id, newObject) => {
-  const response = await axios.put(`${baseUrl}/${id}`, newObject);
+  const response = await axios.put(`${baseUrl}/${id}`, newObject, authConfig());
   return response.data;
 };
 
 const remove = async (id) => {
-  const response = await axios.delete(`${baseUrl}/${id}`);
+  const response = await axios.delete(`${baseUrl}/${id}`, authConfig());
   return response.data;
 };
 
